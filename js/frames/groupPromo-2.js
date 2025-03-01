ac9d0cc9b1342b2a88395121a35c1952168e569a
AModSoul <109749060+AModSoul@users.noreply.github.com>
2025-03-01 03:31:49 -0500

    Adding frames
    Adding Borderless Alt Short and Shattered Glass frames

--- js/frames/groupPromo-2.js
+++ js/frames/groupPromo-2.js
@@ -2,6 +2,7 @@ loadFramePacks([
 	{name:'Regular Frames', value:'PromoOpenHouse'},
 	{name:'Borderless Frames', value:'PromoRegular-1'},
 	{name:'Borderless Frames (Extra Short)', value:'IkoShort'},
+	{name:'Borderless (Alt Short)', value:'borderlessAlt'},
 	{name:'Extended Art Frames', value:'PromoExtended'},
 	{name:'Nickname Frames', value:'PromoNickname'},
 	{name:'Generic Showcase', value:'PromoGenericShowcase'},
--- js/frames/packJapaneseShowcase.js
+++ js/frames/packJapaneseShowcase.js
@@ -1,5 +1,6 @@
 //Create objects for common properties across available frames
 var masks = [{src:'/img/frames/japaneseShowcase/MaskPinlines.png', name:'Pinline'}, {src:'/img/frames/japaneseShowcase/MaskTitle.png', name:'Title'}, {src:'/img/frames/japaneseShowcase/MaskType.png', name:'Type'}, {src:'/img/frames/japaneseShowcase/MaskFramePinline.png', name:'Frame Pinline'}, {src:'/img/frames/japaneseShowcase/MaskBottomFrame.png', name:'BottomFrame'}];
+var masks2 = [{src:'/img/frames/japaneseShowcase/mana/1.png', name:'1 Mana Symbol'}, {src:'/img/frames/japaneseShowcase/mana/2.png', name:'2 Mana Symbol'}, {src:'/img/frames/japaneseShowcase/mana/3.png', name:'3 Mana Symbol'}, {src:'/img/frames/japaneseShowcase/mana/4.png', name:'4 Mana Symbol'}, {src:'/img/frames/japaneseShowcase/mana/5.png', name:'5 Mana Symbol'}, {src:'/img/frames/japaneseShowcase/mana/6.png', name:'6 Mana Symbol'}, {src:'/img/frames/japaneseShowcase/mana/7.png', name:'7 Mana Symbol'}, {src:'/img/frames/japaneseShowcase/mana/8.png', name:'8 Mana Symbol'}, {src:'/img/frames/japaneseShowcase/mana/9.png', name:'9 Mana Symbol'}];
 var bounds = {x:1146/1500, y:1861/2100, width:274/1500, height:140/2100};
 var bounds2 = {x:0.770, y:0.8813, width:0.176, height:0.0618};
 var bounds3 = {x:0, y:0, width:1, height:1};
@@ -25,6 +26,7 @@ availableFrames = [
 	{name:'Vehicle Power/Toughness', src:'/img/frames/japaneseShowcase/pt/v.png', bounds:bounds2},
 	{name:'Colorless Power/Toughness', src:'/img/frames/japaneseShowcase/pt/l.png', bounds:bounds2},
 	
+	{name:'Mana Symbol Backing (10)', src:'/img/frames/japaneseShowcase/mana/10mana.png', masks:masks2, bounds:bounds3},
 	{name:'Border (Place on Bottom Layer)', src:'/img/frames/custom/m15-eighth/regular/Border.png', bounds:bounds3}
 ];
 //disables/enables the "Load Frame Version" button
@@ -46,7 +48,7 @@ document.querySelector('#loadFrameVersion').onclick = async function() {
 	resetWatermark();
 	//text
 	loadTextOptions({
-		mana: {name:'Mana Cost', text:'', y:0.0683, width:0.9190, height:71/2100, oneLine:true, size:71/1638, align:'right', shadowX:-0.001, shadowY:0.0029, manaCost:true, manaSpacing:0,},
+		mana: {name:'Mana Cost', text:'', y:0.0683, width:0.9190, height:71/2100, oneLine:true, size:71/1638, align:'right', manaCost:true, manaSpacing:0,},
 		title: {name:'Title', text:'', x:0.090, y:0.0582, width:0.8292, height:0.0543, outlineWidth:0.0095, oneLine:true, font:'belerenb', size:0.0381, color:'white',},
 		type: {name:'Type', text:'', x:0.0875, y:0.6130, width:0.8000, height:0.0543, oneLine:true, font:'belerenb', outlineWidth:0.0095, size:0.0324, color:'white',},
 		rules: {name:'Rules Text', text:'', x:0.086, y:0.6803, width:0.828, height:0.2265, outlineWidth:0.0095, font:'Plantin MT Pro', size:0.0362, color:'white',},
