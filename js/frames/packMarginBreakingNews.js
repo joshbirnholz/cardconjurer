ea0f9f4119101f2867e190aa23d32199d8193153
AModSoul <109749060+AModSoul@users.noreply.github.com>
2025-03-03 21:47:43 -0500

    Classicshifted Nickname
    Added Classicshifted Nickname option thanks to Proxies by Smaug

--- js/frames/packMarginBreakingNews.js
+++ js/frames/packMarginBreakingNews.js
@@ -1,5 +1,5 @@
 //Create objects for common properties across available frames
-var bounds = {x:-88.5/2010, y:-79/2817, width:2185/2010, height:2975/2817};
+var bounds = {x:-88.5/2010, y:-80/2817, width:2187/2010, height:2978/2817};
 var ogBounds = {x:0, y:0, width:1, height:1};
 //defines available frames
 availableFrames = [
