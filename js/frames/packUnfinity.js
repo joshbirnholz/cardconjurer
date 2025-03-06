a678adacb226e2b25d80c997650b670cdfa7cf5a
AModSoul <109749060+AModSoul@users.noreply.github.com>
2025-03-05 22:04:49 -0500

    Update packUnfinity.js

--- js/frames/packUnfinity.js
+++ js/frames/packUnfinity.js
@@ -30,7 +30,7 @@ document.querySelector('#loadFrameVersion').onclick = async function() {
 	resetWatermark();
 	//text
 	loadTextOptions({
-		title: {name:'Title', text:'', x:0.0854, y:0.0024, width:0.8292, height:0.0639, oneLine:true, font:'belerenb', size:0.0381, color:'white', align:'center'},
+		title: {name:'Titles', text:'', x:0.0854, y:0.0522, width:0.8292, height:0.0543, oneLine:true, font:'belerenb', size:0.0381, color:'white'}
 	});
 }
 //loads available frames
