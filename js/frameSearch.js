e1dc4dc0382dd6b8bd8a9b3c74fda5b4be3203f3
AModSoul <109749060+AModSoul@users.noreply.github.com>
2025-09-07 23:40:42 -0400

    Update frameSearch.js

--- js/frameSearch.js
+++ js/frameSearch.js
@@ -43,6 +43,7 @@ const frameNames = new Map ([
 	['Brawl Legend Crowns', 'Brawl'],
 	// Showcase
 	['Borderless Stellar Sights (EOE)', 'BorderlessStellarSights'],
+	['Through The Ages (FCA)', 'FCA'],
 	['Draconic (TDM)', 'Draconic'],
 	['Ghostfire (TDM)', 'Ghostfire'],
 	['Japan Showcase', 'JapanShowcase'],
