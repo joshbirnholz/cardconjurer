1bce1692ab6b28346745686fcfc9eedbf75967bc
AModSoul <109749060+AModSoul@users.noreply.github.com>
2026-01-15 23:08:43 -0500

    Revert packUBLegendCrowns.js

--- js/frames/packUBLegendCrowns.js
+++ js/frames/packUBLegendCrowns.js
@@ -1,26 +1,18 @@
 //Create objects for common properties across available frames
 var masks = [{src:'/img/frames/m15/crowns/m15MaskLegendCrown.png', name:'Crown Without Pinlines'}, {src:'/img/frames/m15/ub/crowns/m15MaskLegendCrownPinline.png', name:'Crown With Pinlines'}];
-var bounds = {x:1810/2010, y:2290/2814, width:136/2010, height:250/2814};
+var bounds = {x:0.0274, y:0.0191, width:0.9454, height:0.1667};
 //defines available frames
 availableFrames = [
-	{name:'White Transform Arrow', src:'/img/frames/modal/transformAddon/w.png', masks:masks, bounds:bounds},
-	{name:'Blue Transform Arrow', src:'/img/frames/modal/transformAddon/u.png', masks:masks, bounds:bounds},
-	{name:'Black Transform Arrow', src:'/img/frames/modal/transformAddon/b.png', masks:masks, bounds:bounds},
-	{name:'Red Transform Arrow', src:'/img/frames/modal/transformAddon/r.png', masks:masks, bounds:bounds},
-	{name:'Green Transform Arrow', src:'/img/frames/modal/transformAddon/g.png', masks:masks, bounds:bounds},
-	{name:'Multicolored Transform Arrow', src:'/img/frames/modal/transformAddon/m.png', masks:masks, bounds:bounds},
-	{name:'Artifact Transform Arrow', src:'/img/frames/modal/transformAddon/a.png', masks:masks, bounds:bounds},
-	{name:'Vehicle Transform Arrow', src:'/img/frames/modal/transformAddon/v.png', masks:masks, bounds:bounds},
-		{name:'Colorless Transform Arrow', src:'/img/frames/modal/transformAddon/c.png', masks:masks, bounds:bounds},
-
-	{name:'Land Transform Arrow', src:'/img/frames/modal/transformAddon/L.png', masks:masks, bounds:bounds},
-	{name:'White Land Transform Arrow', src:'/img/frames/modal/transformAddon/WL.png', masks:masks, bounds:bounds},
-	{name:'Blue Land Transform Arrow', src:'/img/frames/modal/transformAddon/uL.png', masks:masks, bounds:bounds},
-	{name:'Black Land Transform Arrow', src:'/img/frames/modal/transformAddon/bL.png', masks:masks, bounds:bounds},
-	{name:'Red Land Transform Arrow', src:'/img/frames/modal/transformAddon/rL.png', masks:masks, bounds:bounds},
-	{name:'Green Land Transform Arrow', src:'/img/frames/modal/transformAddon/gL.png', masks:masks, bounds:bounds},
-	{name:'Multicolored Land Transform Arrow', src:'/img/frames/modal/transformAddon/mL.png', masks:masks, bounds:bounds}
-	
+	{name:'White Legend Crown', src:'/img/frames/m15/ub/crowns/m15CrownW.png', masks:masks, bounds:bounds, complementary:9},
+	{name:'Blue Legend Crown', src:'/img/frames/m15/ub/crowns/m15CrownU.png', masks:masks, bounds:bounds, complementary:9},
+	{name:'Black Legend Crown', src:'/img/frames/m15/ub/crowns/m15CrownB.png', masks:masks, bounds:bounds, complementary:9},
+	{name:'Red Legend Crown', src:'/img/frames/m15/ub/crowns/m15CrownR.png', masks:masks, bounds:bounds, complementary:9},
+	{name:'Green Legend Crown', src:'/img/frames/m15/ub/crowns/m15CrownG.png', masks:masks, bounds:bounds, complementary:9},
+	{name:'Multicolored Legend Crown', src:'/img/frames/m15/ub/crowns/m15CrownM.png', masks:masks, bounds:bounds, complementary:9},
+	{name:'Artifact Legend Crown', src:'/img/frames/m15/ub/crowns/m15CrownA.png', masks:masks, bounds:bounds, complementary:9},
+	{name:'Land Legend Crown', src:'/img/frames/m15/ub/crowns/m15CrownL.png', masks:masks, bounds:bounds, complementary:9},
+	{name:'Colorless Legend Crown', src:'/img/frames/m15/crowns/m15CrownC.png', masks:masks, bounds:bounds, complementary:9},
+	{name:'Legend Crown Border Cover', src:'/img/black.png', bounds:{x:0.0394, y:0.0277, width:0.9214, height:0.0177}}
 ];
 //disables/enables the "Load Frame Version" button
 document.querySelector('#loadFrameVersion').disabled = true;
