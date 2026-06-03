c96f4258a9b5e76c60ded9818304e0a5415203d0
Josh Birnholz <josh@birnholz.com>
2026-06-03 14:48:41 -0400

    Add Adventure Time Transform frames

--- js/frames/groupCustom.js
+++ js/frames/groupCustom.js
@@ -76,6 +76,16 @@ loadFramePacks([
 	{name:'Adventure Time Snow', value:'AdventureTimeSnow'},
 	{name:'Adventure Time Adventures', value:'AdventureTimeAdventures'},
 
+	{name:'Adventure Time Transform Front Frames', value:'disabled'},
+	{name:'Adventure Time Transform Front', value:'AdventureTimeRegularTransformFront'},
+	{name:'Adventure Time Enchantments Transform Front', value:'AdventureTimeEnchantmentsTransformFront'},
+	{name:'Adventure Time Snow Transform Front', value:'AdventureTimeSnowTransformFront'},
+
+	{name:'Adventure Time Transform Back Frames', value:'disabled'},
+	{name:'Adventure Time Transform Back', value:'AdventureTimeRegularTransformBack'},
+	{name:'Adventure Time Enchantments Transform Back', value:'AdventureTimeEnchantmentsTransformBack'},
+	{name:'Adventure Time Snow Transform Back', value:'AdventureTimeSnowTransformBack'},
+
 	{name:'Showcase Frame Varients', value:'disabled'},
 	{name:'Short Neon (NEO)', value:'NEONeonShort'},
 	{name:'Colored Golden Age (SNC)', value:'SNCGildedColored'},
