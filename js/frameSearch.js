c106287cd1190be5181cd82dc780580ed42e492f
Josh Birnholz <josh@birnholz.com>
2026-06-24 13:24:04 -0400

    Update Horizontal Japanese Mystical Archive with official frame

--- js/frameSearch.js
+++ js/frameSearch.js
@@ -293,7 +293,7 @@ const frameNames = new Map ([
 	['Colored Golden Age (SNC)', 'SNCGildedColored'],
 	['Textless Golden Age (SNC)', 'SNCGildedTextless'],
 	['Textless Equinox (MID)', 'EquinoxTextless'],
-	['Horizontal Japanese Mystical Archive (STA)', 'MysticalArchiveJPHorizontal'],
+	['Horizontal Japanese Mystical Archive (STA)', 'MysticalArchiveJPEN'],
 	['Brawl Legend Crowns', 'Brawl'],
 	['Cartoony - Sheepwave', 'Cartoony'],
 	['Neon - Elry', 'CustomNeon'],
